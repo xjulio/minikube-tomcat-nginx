20d5f0b11669b75eec37a37baf920379ffabb7c2
Julio Cesar Damasceno <45015654+xjulio@users.noreply.github.com>
2019-01-14 17:42:14 -0300

    Chaneg tag names

--- run.sh
+++ run.sh
@@ -12,10 +12,10 @@ fi
 eval $(minikube docker-env)
 
 cd $DIR/docker
-#Build Tomcat custom image
+#Build tomcat custom image
 docker image build -t tomcat:8.5-a1 . -f tomcat.Dockerfile
 
-#Build Nginx custom image
+#Build nginx custom image
 docker image build -t nginx:a1 . -f nginx.Dockerfile
 
 cd $DIR
